b4cf1865534b38eba69e6dce2f792c300e955216
Venkatasandeep Gengineri <venkatasandeepgengineri@Venkatasandeeps-MacBook-Pro.local>
2017-10-29 16:04:16 -0400

    Halfway done with new feature, Tested so far so good.

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+function aBigNewFeature() {
+  console.log('Writing a lot of code');
+  console.log('Halfway done with the coding as of now.');
+}
+
 function something() {
    console.log('I\'m something');
 }
 console.log('hello world!');
 something();
+aBigNewFeature();
